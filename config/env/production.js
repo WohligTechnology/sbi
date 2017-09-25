2b5f3daf53600dd223ddf7a468a6a83eaa37eb7a
pratik <pratik.shah@wohlig.com>
2017-09-25 12:52:50 +0530

    https

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 443,
-  realHost: "https://cingulariti.com",
+  realHost: "http://cingulariti.com",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
   /***************************************************************************
