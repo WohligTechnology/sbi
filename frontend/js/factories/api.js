c47eff3d5bccfa925726be8696be00498e7f321d
pratik <pratik.shah@wohlig.com>
2017-09-25 13:50:33 +0530

    api

--- frontend/js/factories/api.js
+++ frontend/js/factories/api.js
@@ -1,7 +1,7 @@
 myApp.factory('apiService', function ($http, $q, $timeout,$httpParamSerializer,$httpParamSerializerJQLike) {
     adminurl2 = "http://35.161.160.7:8095/";
     var adminurl3 = "http://localhost/api/";
-    var adminurl3 = "https://cingulariti.com:443/api/";
+    var adminurl3 = "http://cingulariti.com:443/api/";
     
     return {
 
