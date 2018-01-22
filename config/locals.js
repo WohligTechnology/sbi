2d0490ba724b15314c6897d55091f282eef2b09b
pratik <pratik.shah@wohlig.com>
2018-01-22 19:25:14 +0530

    ssl

--- config/locals.js
+++ config/locals.js
@@ -1,8 +1,7 @@
 module.exports = {
     hookTimeout: 30000000,
     ssl: {
-        key: require('fs').readFileSync(__dirname + '/ssl/newprivatekey1.pem'),
-        cert: require('fs').readFileSync(__dirname + '/ssl/newcert1.pem')
+        key: require('fs').readFileSync(__dirname + '/ssl/privatekey.pem'),
+        cert: require('fs').readFileSync(__dirname + '/ssl/cert.pem')
     },
-    //explicitHost: process.env.HOST || 'localhost'
 };
